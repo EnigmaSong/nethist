32741835a5caadc7f601f5b3d44504293df1d5e8
Youngseok Song <y.song.stat@gmail.com>
2022-11-19 17:30:46 +0100

    internal function for Hamming distance RcppArmadillo

--- src/nethist_utils.cpp
+++ src/nethist_utils.cpp
@@ -13,6 +13,25 @@ double ffct(int n, int k){
   return (double) result;
 }
 
+// [[Rcpp::export(.hamming_dist_adj_mat)]]
+arma::mat hamming_dist_adj_mat(const arma::mat &A){
+  //To compute hamming distance matrix for adjacency matrix from pairs of column vectors
+  //It is faster than dist(method="manhattan"), which uses primitive C function, because 
+  //i) it does not have error handling in the C primitive (= no if statement for error handling like dist())
+  //ii) it runs smaller number of arithmetic operations.
+  int n = A.n_cols;
+  arma::mat result(n,n);
+  
+  for(int i = 0; i < n; i++){
+    for(int j = i+1; j <n; j++){
+      result(i,j) += sum(A.col(i)!= A.col(j));
+    }
+  }
+  result = symmatu(result);
+  
+  return result;
+}
+
 arma::vec sample(const arma::vec& x, const int& size, const bool& replace){
   return Rcpp::RcppArmadillo::sample(x, size, replace);
 }
