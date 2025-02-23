0afcf71f3cbda46135c2383f35e477fab424afad
Youngseok Song <20687349+EnigmaSong@users.noreply.github.com>
2025-02-23 17:19:01 -0500

    Bug fix: For (S.57) of the paper, instead of NaN, return 0 when the sub-sampled network is empty by changing the denominator to be max(1, number of /.).

--- src/net_summary_subsample_adj.cpp
+++ src/net_summary_subsample_adj.cpp
@@ -34,8 +34,7 @@ arma::mat net_summary_subsample_adj(const arma::mat &A, const arma::vec &subsamp
 
     sample_ind = sample(total_ind, (int)subsample_size, false);
     deg_A_sub = sum(A(sample_ind,sample_ind), 1);
-    
-    result(r,0) = (1/2.0*sum(deg_A_sub%(deg_A_sub-1.0))) / ((subsample_size-2)/2*sum(deg_A_sub)); // (S.57) in Maugis et al. (2017)
+    result(r,0) = (1/2.0*sum(deg_A_sub%(deg_A_sub-1.0))) / std::max(1.0, (subsample_size-2)/2*sum(deg_A_sub)); // (S.57) in Maugis et al. (2017)
     //  Compute k-cycle ratio defined in (S.58)
     cycle_ratio = count_k_cycle(A(sample_ind,sample_ind), max_cycle_order);
     cycle_ratio = cycle_ratio/denoms.col(temp_ind);
